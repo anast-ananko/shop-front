b0136367b25d9e1f548c8351c41d8e9b55c82f84
Artur Tamashevich <tamashevich.artur@gmail.com>
2026-05-19 16:09:40 +0200

    feat: add basic routes + lazy routes

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Slider } from '../../shared/components/slider/slider';
+import { BooksService } from '../../core/books-service/books-service';
+import { ProductCard } from '../../shared/components/product-card/product-card';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ProductCard, Slider],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {}
+export class Home {
+    private bookService = inject(BooksService);
+  protected readonly filteredBooks = this.bookService.filteredBooks;
+}
---
+++ src/app/routers/auth.routes.ts
@@ -0,0 +1,33 @@
+import {Routes} from "@angular/router";
+import { Auth } from "../pages/auth/auth";
+
+export const authRoutes: Routes = [
+  {
+    path: "auth",
+    component: Auth,
+    children: [
+      {
+        path: "registration",
+        loadComponent: async () => {
+          const m = await import("../pages/auth/components/registration/registration");
+          return m.Registration;
+        },
+      },
+      {
+        path: "sign-in",
+        loadComponent: async () => {
+          const m = await import("../pages/auth/components/sign-in/sign-in");
+          return m.SignIn;
+        },
+      },
+      {
+        path: "profile",
+        loadComponent: async () => {
+          const m = await import("../pages/auth/components/profile/profile");
+          return m.Profile;
+        },
+      },
+    ],
+  },
+];
+
