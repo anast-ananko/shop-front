2d087a9760e262151ffec261b6fa4dc03fee95bd
Artur Tamashevich <tamashevich.artur@gmail.com>
2026-05-22 17:42:43 +0200

    refactor: move books loading to app initializer - moved books loading logic from app component to app initializer - removed unnecessary subscriptions from app component - improved application startup flow and code readability

--- src/app/core/services/app-initializer.service.ts
+++ src/app/core/services/app-initializer.service.ts
@@ -2,16 +2,19 @@ import { inject, Injectable } from '@angular/core';
 import { firstValueFrom } from 'rxjs';
 
 import { AuthService } from '../http/services/auth/auth.service';
+import { BooksService } from '../books-service/books-service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AppInitializerService {
   private authService = inject(AuthService);
+  private booksService = inject(BooksService);
 
   async load(): Promise<void> {
     await firstValueFrom(this.authService.getAccessToken());
-
+    await firstValueFrom(this.booksService.getBooks());
+    
     this.authService.initAuthFlow();
   }
 }
