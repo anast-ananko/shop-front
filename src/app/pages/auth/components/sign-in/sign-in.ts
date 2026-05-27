957e1f94153273e6d1b37dabcaca1df151714c0e
Artur Tamashevich <tamashevich.artur@gmail.com>
2026-05-27 12:58:51 +0200

    feat: add login page structure and reuse registration page styles

--- src/app/pages/auth/components/sign-in/sign-in.ts
+++ src/app/pages/auth/components/sign-in/sign-in.ts
@@ -1,9 +1,60 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-sign-in',
-  imports: [],
+  imports: [CommonModule,
+    ReactiveFormsModule,
+    RouterModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatCheckboxModule,
+    MatSelectModule,
+    MatFormFieldModule],
   templateUrl: './sign-in.html',
   styleUrl: './sign-in.scss',
 })
-export class SignIn {}
+export class SignIn {
+  private fb = inject(FormBuilder);
+
+    serverError = signal<string | null>(null);
+
+    form = this.fb.nonNullable.group({
+      email: ['', [Validators.required, Validators.pattern(/^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/)]],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]+$/),
+        ],
+      ],
+    });
+
+
+
+
+    submit() {
+      if (this.form.invalid) return;
+
+
+      const form = this.form.getRawValue();
+
+      const signupPayload = {
+        email: form.email,
+        password: form.password,
+      };
+
+      console.log(signupPayload);
+    }
+}
