8d76c0e550ecaed0f93e5e13274dead9d9bc4c9c
Artur Tamashevich <tamashevich.artur@gmail.com>
2026-06-06 15:11:34 +0200

    feat: add book rendering and product cards to the Search Book component

--- src/app/pages/search/search.ts
+++ src/app/pages/search/search.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { BooksService } from '../../core/services/books-service/books-service';
+import { ProductCard } from '../../shared/components/product-card/product-card';
 
 @Component({
   selector: 'app-search',
-  imports: [],
+  imports: [ProductCard],
   templateUrl: './search.html',
   styleUrl: './search.scss',
 })
-export class Search {}
+export class Search {
+  private bookService = inject(BooksService);
+  books = this.bookService.filteredBooks;
+}
