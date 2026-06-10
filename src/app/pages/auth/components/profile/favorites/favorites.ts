5c31d1cb090efe074b10098d66830dea54b411f6
Artur Tamashevich <tamashevich.artur@gmail.com>
2026-06-10 22:22:57 +0200

    refactor: extract reusable paginated books catalog component

--- src/app/pages/auth/components/profile/favorites/favorites.ts
+++ src/app/pages/auth/components/profile/favorites/favorites.ts
@@ -1,31 +1,14 @@
-import { Component, computed, inject, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { BooksService } from '../../../../../core/services/books-service/books-service';
-import { ProductCard } from '../../../../../shared/components/product-card/product-card';
-import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { PaginatedBooksCatalog } from '../../../../../shared/components/paginated-books-catalog/paginated-books-catalog';
 
 @Component({
   selector: 'app-favorites',
-  imports: [ProductCard, MatPaginatorModule],
+  imports: [PaginatedBooksCatalog],
   templateUrl: './favorites.html',
   styleUrl: './favorites.scss',
 })
 export class Favorites {
   private readonly booksService = inject(BooksService);
   favoriteBooks = this.booksService.favoriteBooks;
-
-  pageIndex = signal(0);
-  pageSizeOptions = [4, 8, 12, 16];
-  pageSize = signal(4);
-
-  paginatedBooks = computed(() => {
-    const start = this.pageIndex() * this.pageSize();
-    const end = start + this.pageSize();
-
-    return this.favoriteBooks().slice(start, end);
-  });
-
-  handlePageEvent(e: PageEvent) {
-    this.pageSize.set(e.pageSize);
-    this.pageIndex.set(e.pageIndex);
-  }
 }
