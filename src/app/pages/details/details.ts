a92a8eaaf186187a6138a747ba85f0797d227841
Artur Tamashevich <tamashevich.artur@gmail.com>
2026-06-13 17:52:11 +0200

    refactor: replace home navigation with browser back navigation

--- src/app/pages/details/details.ts
+++ src/app/pages/details/details.ts
@@ -1,20 +1,26 @@
 import { Component, inject } from '@angular/core';
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { BooksService } from '../../core/services/books-service/books-service';
 import { MatButtonModule } from '@angular/material/button';
 import { CurrencyPipe } from '@angular/common';
 import { MatIcon } from "@angular/material/icon";
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-details',
-  imports: [MatButtonModule, RouterLink, CurrencyPipe, MatIcon],
+  imports: [MatButtonModule, CurrencyPipe, MatIcon],
   templateUrl: './details.html',
   styleUrl: './details.scss',
 })
 export class Details {
   private readonly bookService = inject(BooksService);
   private readonly route = inject(ActivatedRoute);
+  private location = inject(Location);
 
   currentId = this.route.snapshot.paramMap.get('id') || '';
   readonly book = this.bookService.getBookById(this.currentId);
+
+  goBack(): void {
+    this.location.back();
+  }
 }
