1553ca3d960b5db4cd9c39ca04d5f84a140b5ec5
Artur Tamashevich <58971909+iskonplus@users.noreply.github.com>
2026-06-14 18:10:10 +0200

    Remove unused RouterLink import from details.ts

--- src/app/pages/details/details.ts
+++ src/app/pages/details/details.ts
@@ -1,5 +1,5 @@
 import { Component, inject, ChangeDetectionStrategy } from '@angular/core';
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { BooksService } from '../../core/services/books-service/books-service';
 import { MatButtonModule } from '@angular/material/button';
 import { CurrencyPipe } from '@angular/common';
