ce6889cc92fd8dd92a8c479fc82486dc5c975c49
Nastassia Ananka <anast.ananko@gmail.com>
2026-06-16 18:13:29 +0200

    feat: add abous us page content

---
+++ src/app/constants/team-data.ts
@@ -0,0 +1,30 @@
+import { TeamMember } from '../pages/about-us/about-us';
+
+export const TEAM_MEMBERS: TeamMember[] = [
+  {
+    name: 'Nastassia Ananka',
+    role: 'Frontend Developer / Team Lead',
+    bio: `
+      Frontend developer and team lead who enjoys turning complex ideas 
+      into clear, maintainable solutions. Focused on architecture, code quality, 
+      and building features that scale.
+      Leads the authentication and profile domain, sets up project workflows, 
+      and helps the team stay aligned and productive.
+    `,
+    photo: 'assets/images/Nastassia.jpg',
+    github: 'https://github.com/anast-ananko',
+  },
+  {
+    name: 'Artur Tamashevich',
+    role: 'Frontend Developer',
+    bio: `
+      Frontend developer passionate about clean code and reactive 
+      programming. Enjoys working with Angular and RxJS to build efficient, 
+      reusable components.
+      Constantly learning and improving through hands-on problem solving 
+      and real project experience.
+    `,
+    photo: 'assets/images/Artur.jpg',
+    github: 'https://github.com/iskonplus',
+  },
+];
